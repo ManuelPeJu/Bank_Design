8f1cc31838f244b728d6d0c40edf3539448d9167
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-06-20 23:20:52 +0200

    Componente Stats 100%

--- Bank_modern_app/src/components/Hero.jsx
+++ Bank_modern_app/src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
         <div className='absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient' />
       </div>
 
-      <div className={` ss:hidden ${styles.flexCenter}`}>
+      <div className={` ss:hidden ${styles.flexCenter} `}>
         <GetStarted />
       </div>
     </section>
