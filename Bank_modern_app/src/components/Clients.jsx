65d8f7d737d245b2cd2ee3df8c2e43dd59f99ce0
Manuel Pérez Jurado <perezjuradomanuel@gmail.com>
2023-06-27 18:27:14 +0200

    pequeño arreglo en Clients(css) y CTA 100% done

--- Bank_modern_app/src/components/Clients.jsx
+++ Bank_modern_app/src/components/Clients.jsx
@@ -7,9 +7,9 @@ const Clients = () => {
     <section className={`${styles.flexCenter} my-4`}>
       <div className={`${styles.flexCenter} flex-wrap w-full`}>
         {
-          clients.map((client, index) => {
+          clients.map((client) => {
             return (
-              <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px]`}>
+              <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] m-5`}>
                 <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
               </div>
             )
